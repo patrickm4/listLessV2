c71dadff7d900dddbc3b1d6873515e67c567a040
Patrick <patrickmoreno444@gmail.com>
2023-04-02 14:56:15 -0700

    master feat: move over html elements/turn description into component WIP

---
+++ src/components/description.vue
@@ -0,0 +1,43 @@
+<template>
+    <textarea rows="5" cols="50">
+        {{ desc }}
+    </textarea>
+</template>
+
+<script>
+
+export default {
+    props: [
+        'cardname'
+    ],
+    data () {
+        return {
+        }
+    },
+    computed: {
+        desc () {
+            return `
+            <link rel="preconnect" href="https://fonts.gstatic.com">
+            <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@300;600&display=swap" rel="stylesheet">
+
+
+            <div style="display:flex; justify-content: center; align-items: center; background-color: rgba(0,0,0,0.175); padding: 10px 15px;">
+            <div style="font-size: 16pt; font-weight:600; font-family: 'Dosis', sans-serif; width: 80%; overflow-wrap: break-word;">${this.cardname || 'NO NAME'}</div>
+
+            <div style="margin-left: 2rem;">
+            <ul>
+            <li style="font-size: 12pt; margin-bottom: 1em; font-weight:300; font-family: 'Dosis', sans-serif;">One Card.</li>
+
+            <li style="font-size: 12pt; margin-bottom: 1em; font-weight:300; font-family: 'Dosis', sans-serif;">Ships inside penny sleeve and top loader.</li>
+
+            <li style="font-size: 12pt; margin-bottom: 1em; font-weight:300; font-family: 'Dosis', sans-serif;">Orders are usually shipped in a plain white envelope. Depending on the value and/or quantity of cards, orders will be shipped either in a bubble mailer or box.</li>
+
+            <li style="font-size: 12pt; font-weight:300; font-family: 'Dosis', sans-serif;">Card shipped out from BC, Canada</li>
+            </ul>
+            </div>
+            </div>
+            `
+        }
+    }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,35 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <h2>ListLessV2</h2>
+    <h3>Drag and drop card</h3>
+    <div class="image-box">
+    </div>
+    <div class="filename"></div>
+    <div>
+      <button id="copy-filename">Copy</button>
+      <button id="generate-btn">Force Generate</button>
+      <input type="radio" id="pokemon" name="card-type" value="poke" checked>
+      <label for="pokemon">Poke</label>
+      <input type="radio" id="yugioh" name="card-type" value="yugi">
+      <label for="yugioh">Yugi</label>
+    </div>
+
+    <br />
+
+    <div id="research-link-box"></div>
+
+    <br />
+    <br />
+
+    <div class="desc-header">
+      <h5>Description HTML</h5>
+      <div id="copy-desc">Copy</div>
+    </div>
+    <!-- <textarea id="innerTextOutput" rows="6" cols="30" readonly>...</textarea> -->
+    <Description />
+
+    <div id="name-btns"></div>
 </template>
+
+<script setup lang="ts">
+import Description from '../components/description.vue'
+</script>
