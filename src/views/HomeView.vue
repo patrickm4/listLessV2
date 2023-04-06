ba521cd7515cc9e40dc9e4f1a5d836ac685838c8
Patrick <patrickmoreno444@gmail.com>
2023-04-05 21:58:24 -0700

    master: feat: WIP: create file drop area

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
     <h2>ListLessV2</h2>
     <h3>Drag and drop card</h3>
+    <button @click="clearDrop">Clear Drop</button>
+    <input type="file" @change="drop" class="drop-input" ref="dropBox" />
+    <!-- <div class="drop-area" style="background-color: antiquewhite;border:2px dotted #fff; width: 100%; height: 20rem"></div> -->
     <div class="image-box">
     </div>
     <div class="filename"></div>
@@ -25,6 +28,31 @@
     <div id="name-btns"></div>
 </template>
 
-<script setup lang="ts">
+<script>
 import Description from '../components/description.vue'
+
+export default {
+  components: {
+    Description
+  },
+  methods: {
+    drop (e) {
+      console.log("drop method ", e)
+    },
+    clearDrop () {
+      this.$refs.dropBox.value = ''
+    }
+  }
+}
 </script>
+
+<style>
+.drop-input {
+  width:100%; 
+  height: 10rem;
+  border: 5px dotted #fff;
+}
+::-webkit-file-upload-button {
+   display: none;
+}
+</style>
