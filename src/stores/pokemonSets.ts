7733d6f844fb4a9e7ee8185c2247db6ef9340e2b
patrickm4 <patrickmoreno444@gmail.com>
2023-04-16 21:58:02 -0700

    master fix: minimize state return for pokemone set store

--- src/stores/pokemonSets.ts
+++ src/stores/pokemonSets.ts
@@ -6,16 +6,7 @@ pokemon.configure({apiKey: config.pokeApiKey})
 
 export const usePokemonSetsStore = defineStore('pokeSets', {
     state: () => {
-        // if (window.localStorage.getItem('pokemon_sets')) {
-        //     return { 
-        //         sets: JSON.parse(window.localStorage.getItem('pokemon_sets') || '[]')
-        //     }
-        // } else {
-        //     console.error("No pokemon sets in store!")
-        //     return { 
-        //         sets:[]
-        //     }
-        // }
+        if (!window.localStorage.getItem('pokemon_sets')) console.error("No pokemon sets in store!")
 
         return { 
             sets: JSON.parse(window.localStorage.getItem('pokemon_sets') || '[]')
